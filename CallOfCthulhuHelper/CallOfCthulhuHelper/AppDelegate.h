ef4ec75b4867dbc7ec611c812f4e57fd0229f19f
Knizhnik Yevgeniy <vincentwerewolf@gmail.com>
2014-06-10 21:06:02 +0300

    initial commit

---
+++ CallOfCthulhuHelper/CallOfCthulhuHelper/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  CallOfCthulhuHelper
+//
+//  Created by Kroshmelot on 10.06.14.
+//  Copyright (c) 2014 Kroshmelot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ CallOfCthulhuHelper/CallOfCthulhuHelper/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CallOfCthulhuHelper
+//
+//  Created by Kroshmelot on 10.06.14.
+//  Copyright (c) 2014 Kroshmelot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
